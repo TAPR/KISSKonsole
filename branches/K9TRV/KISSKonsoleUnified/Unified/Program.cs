b2931856b5471ec911a3574773f30953b3b7619f
k9trv <k9trv@bc95016a-09c1-4540-80c3-a564db75a9aa>
2012-09-09 16:58:35 +0000

    version 1.1.19.  Add ability to use a command line parameter to override where kk.csv is to be located.  allows multiple copies of KISS to co-exist

--- branches/K9TRV/KISSKonsoleUnified/Unified/Program.cs
+++ branches/K9TRV/KISSKonsoleUnified/Unified/Program.cs
@@ -10,11 +10,27 @@ namespace KISS_Konsole
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            // allow a user to pass command line arguments to do things like set a non-default application data directory
+            // so that multiple discrete instances of KISS Konsole could be run on the system.  Note that the installer only allows
+            // one instance to be 'installed'.  But others could be created by copying the installed instance...
+            string appDataDir = "";
+            // NOTE: these args do NOT include the program name!
+            if (args != null)
+            {
+                if (args.Length >= 2)
+                {
+                    if (args[0].ToUpper().Equals("APPDATADIR"))
+                    {
+                        appDataDir = args[1];
+                    }
+                }
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1(appDataDir));
         }
     }
 }
--- branches/K9TRV/KISSKonsoleUnified/Unified/Properties/AssemblyInfo.cs
+++ branches/K9TRV/KISSKonsoleUnified/Unified/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.18.0")]
-[assembly: AssemblyFileVersion("1.1.18.0")]
+[assembly: AssemblyVersion("1.1.19.0")]
+[assembly: AssemblyFileVersion("1.1.19.0")]
