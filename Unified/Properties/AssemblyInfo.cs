c2c469fb265429fb1b9f51ca002bcdafe47f8dd7
k9trv <k9trv@bc95016a-09c1-4540-80c3-a564db75a9aa>
2012-12-15 18:56:20 +0000

    version 1.1.24: metis now is in version 2.2.  Amend CheckVersion to understand this version.

--- Unified/Properties/AssemblyInfo.cs
+++ Unified/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.1.23.0")]
-[assembly: AssemblyFileVersion("1.1.23.0")]
+[assembly: AssemblyVersion("1.1.24.0")]
+[assembly: AssemblyFileVersion("1.1.24.0")]
